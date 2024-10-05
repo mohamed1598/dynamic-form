984a80ce0f83e44e0fefbc7929cb9be6cc5d8278
mohamed elshafey <elshafeym170@gmail.com>
2024-10-05 16:07:20 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
-import { AddEditFormComponent } from './add-edit-form/add-edit-form.component';
+import { AddEditFormComponent } from './form/add-edit-form/add-edit-form.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UsersComponent },
+    { path: 'users/:id', component: UsersComponent },
     { path: 'design', component: AddEditFormComponent },
     { path: 'design/edit/:id', component: AddEditFormComponent }
 ];
